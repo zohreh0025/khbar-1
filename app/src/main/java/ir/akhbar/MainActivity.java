95ce46dfcc2071c45e765be21840fdca167385f7
KabiriAndroid <kabirinouri@yahoo.com>
2020-01-02 16:30:40 +0330

    Add ability to increase the counter

--- app/src/main/java/ir/akhbar/MainActivity.java
+++ app/src/main/java/ir/akhbar/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentNavigation.navigate(this, new NewsListFragment(), 0);
     }
 
 }
--- app/src/main/java/ir/akhbar/NewsListFragment.java
+++ app/src/main/java/ir/akhbar/NewsListFragment.java
@@ -6,13 +6,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class NewsListFragment extends Fragment {
+
+    private int counter = 0;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle fragmentBundle = getArguments();
+        counter = fragmentBundle.getInt("Counter");
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_news_list, container, true);
+        return inflater.inflate(R.layout.fragment_news_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView counterTextView = (TextView) view.findViewById(R.id.counterTextView);
+        counterTextView.setText(String.valueOf(counter));
+
+        Button plusButton = (Button) view.findViewById(R.id.plusButton);
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentNavigation.navigate(getActivity(), new NewsListFragment(), counter);
+            }
+        });
     }
 }
