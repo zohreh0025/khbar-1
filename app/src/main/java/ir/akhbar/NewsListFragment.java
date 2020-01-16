97583b276cd0651ac1645a8b674a7e964f82ddd2
kabiri <rshahi23@yahoo.com>
2020-01-16 16:03:55 +0330

    Handle progress and server failure

--- app/src/main/java/ir/akhbar/NewsListFragment.java
+++ app/src/main/java/ir/akhbar/NewsListFragment.java
@@ -8,12 +8,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class NewsListFragment extends Fragment {
+
+    private ProgressBar progress;
+    private RelativeLayout failureView;
+    private RecyclerView newsRecycler;
 
     @Nullable
     @Override
@@ -25,15 +33,33 @@ public class NewsListFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        final RecyclerView newsRecycler = (RecyclerView) view.findViewById(R.id.newsRecycler);
-        newsRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        Networking networking = new Networking();
+        final Networking networking = new Networking();
+
+        progress = (ProgressBar) view.findViewById(R.id.progress);
+        failureView = (RelativeLayout) view.findViewById(R.id.failureView);
+        newsRecycler = (RecyclerView) view.findViewById(R.id.newsRecycler);
+        newsRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        Button retryButton = (Button) view.findViewById(R.id.retryButton);
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                failureView.setVisibility(View.GONE);
+                progress.setVisibility(View.VISIBLE);
+                fetchData(networking);
+            }
+        });
+
+        fetchData(networking);
+    }
+
+    private void fetchData(Networking networking) {
         networking.getServer()
                 .getNewsList()
                 .enqueue(new Callback<ServerResponse>() {
                     @Override
                     public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
+                        progress.setVisibility(View.GONE);
                         ServerResponse serverResponse = response.body();
                         NewsAdapter adapter = new NewsAdapter(serverResponse.getRoot(), new NewsItemClickListener() {
                             @Override
@@ -56,7 +82,8 @@ public class NewsListFragment extends Fragment {
 
                     @Override
                     public void onFailure(Call<ServerResponse> call, Throwable t) {
-
+                        progress.setVisibility(View.GONE);
+                        failureView.setVisibility(View.VISIBLE);
                     }
                 });
     }
